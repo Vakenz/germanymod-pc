9c7f329f3cb81183c6e5ba4e4cde107828982cb9
SotoSapi1 <163963817+SotoSapi2@users.noreply.github.com>
2026-08-30 03:46:06 +0700

    Updated invalid patterns

--- src/runtime/game/import/PointerFunctions.cpp
+++ src/runtime/game/import/PointerFunctions.cpp
@@ -457,17 +457,17 @@ namespace ContentKeyRegister
 {
 	Pointer<IL2CPP::Object* ()> GetInstance(
 		"ContentKeyRegister",
-		0x47
+		0x4a
 	);
 
 	Pointer<IL2CPP::List<IL2CPP::String*>* (IL2CPP::Object* instance, OfferItemType type)> GetRegisterList(
 		"ContentKeyRegister",
-		0x54
+		0x57
 	);
 
 	Pointer<int(IL2CPP::Object* instance, OfferItemType type, IL2CPP::String* key)> GetItemIndex(
 		"ContentKeyRegister",
-		0x4f
+		0x52
 	);
 }
 
